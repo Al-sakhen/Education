7536cd3bcb0bd4bd76562af3b3fcce5173289229
DeekCS <31720001079@std.bau.edu.jo>
2021-08-11 22:51:04 +0300

    fix the footer & Shadow & Cards

--- resources/views/course_details.blade.php
+++ resources/views/course_details.blade.php
@@ -1,7 +1,7 @@
 @include('header')
 
 <!-- End Header -->
-<main id="main" data-aos="fade-up" style="height: 100vh;">
+<main id="main" data-aos="fade-up" class="flex-column min-vh-100">
     <!-- ======= Breadcrumbs Section ======= -->
     <section class="breadcrumbs">
     <div class="container">
@@ -65,42 +65,11 @@
                 </div>
 
 
-<<<<<<< HEAD
                 <div class="col-lg-3 mix tests all">
                     <div class="materials">
                         <a href="" class="card education">
                         <div class="circle">
                             <img class="" src="{{asset('assets/img/svg/download-file1.png')}}" alt="" srcset="">
-=======
-            <div class="row">
-                <div class="col-lg-6 ">
-
-              
-
-                    <!-- <div class="career-search mb-60"> -->
-                        <!-- <div class="filter-result"> -->
-
-                            <!-- <p class="mb-30 ff-montserrat">Total Job Openings : 89</p> -->
-                        <!-- @foreach ($details as $d)
-
-                        <div class="job-box d-md-flex align-items-center justify-content-center mb-25">
-                                <div class="job-left my-4 d-md-flex align-items-center flex-wrap">
-                                    <div class="img-holder mr-md-4 mb-md-0 mb-4 mx-auto mx-md-0  d-lg-flex">
-                                    <img class="w-75" src="{{asset('assets/img/desc/folder1.svg')}}" alt="" srcset="">
-
-                                    </div>
-                                    <div class="job-content">
-
-                                        <ul class="d-md-flex flex-wrap text-capitalize ff-open-sans  justify-content-between">
-                                            <h5 class="text-center text-md-left mt-4">{{$d->name}}</h5>
-
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="job-right my-4 flex-shrink-0">
-                                    <a href="{{url('download/'.$d->file_path)}}" class="btn d-block w-100 d-sm-inline-block btn-light">تحميل</a>
-                                </div>
->>>>>>> 115fb21081f96aee276b48160231038625a9035d
                         </div>
                         <p>CH1</p>
                         </a>
@@ -117,24 +86,17 @@
                         <div class="circle">
                             <img class="" src="{{asset('assets/img/svg/download-file1.png')}}" alt="" srcset="">
                         </div>
-<<<<<<< HEAD
                         <p>CH1</p>
                         </a>
-=======
-
->>>>>>> 115fb21081f96aee276b48160231038625a9035d
                     </div>
 
-                    
-
-                    
 
 
-                </div>
-                <div class="col-lg-6">
-                <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLCInYL3l2AajFAiw4s1U4QbGszcQ-rAb3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+
+
 
                 </div>
+
         <!-- url('download/'.$d->file_path) -->
         <!-- $d->name -->
     </div>
